1f3fe8db91dc7a2f5be77378ced317520b6a1446
Brian <brianbaso@gmail.com>
2019-07-15 18:57:46 -0700

    working on react router

---
+++ src/Idea.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Idea extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {/*<Idea/>*/}
+      </div>
+    );
+  }
+}
--- src/NavBar.js
+++ src/NavBar.js
@@ -8,6 +8,8 @@ import {
   NavItem,
   NavLink
  } from 'reactstrap';
+import { Link } from "react-router-dom";
+
 import nqlogo from './img/neuroquery-logo.svg';
 import loginButton from './img/login-button.svg';
 import signupButton from './img/signup-button.svg';
@@ -30,24 +32,24 @@ export default class NavBar extends React.Component {
     return (
       <div>
         <Navbar light expand="md">
-          <NavbarBrand href="/"><img className="navbar-logo" src={nqlogo}/></NavbarBrand>
+          <NavbarBrand href="/"><img className="navbar-logo" alt="Neuroquery" src={nqlogo}/></NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem className="navbar-text-buttons">
-                <NavLink className="navbar-text" href="/components/">Community</NavLink>
+                <Link to="/IdeaLibrary/" className="navbar-text">Library</Link>
               </NavItem>
               <NavItem className="navbar-text-buttons">
                 <NavLink className="navbar-text" href="https://github.com/reactstrap/reactstrap">Game</NavLink>
               </NavItem>
               <NavItem>
                 <NavLink className="navbar-buttons-parent-left" href="https://github.com/reactstrap/reactstrap">
-                  <img className="navbar-buttons" src={loginButton}/>
+                  <img className="navbar-buttons" alt="Log In" src={loginButton}/>
                 </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink className="navbar-buttons-parent-right" href="https://github.com/reactstrap/reactstrap">
-                  <img className="navbar-buttons" src={signupButton}/>
+                  <img className="navbar-buttons" alt="Sign up" src={signupButton}/>
                 </NavLink>
               </NavItem>
             </Nav>
