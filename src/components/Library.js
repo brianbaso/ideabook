261ad8ae401fc8034e259e6a397eb4821c72948e
Brian <brianbaso@gmail.com>
2019-08-09 22:21:57 -0400

    unique id for child elements

--- src/components/Library.js
+++ src/components/Library.js
@@ -18,10 +18,11 @@ export default class Library extends React.Component {
       .collection("private-ideas");
     let ideas = [];
 
+    // Create the ideas array and push the firestore doc id into each element
     dbRef.get()
       .then((snapshot) => {
         snapshot.forEach(doc => {
-          ideas.push(doc.data());
+          ideas.push({ ...doc.data(), id: doc.id });
         });
         this.setState({ ideas : ideas });
       })
@@ -33,9 +34,10 @@ export default class Library extends React.Component {
   render() {
     const libraryCards = [];
 
-    this.state.ideas.forEach((idea, i) => {
+    this.state.ideas.forEach((idea) => {
+      console.log(idea);
       libraryCards.push(
-        <div className="my-2 rounded" key={i} id="libraryParentDiv">
+        <div className="my-2 rounded" key={idea.id} id="libraryParentDiv">
           <Toast id="libraryParentToast">
             <ToastHeader id="libraryHeaderToast">
               {idea.submissionTags[0]}
