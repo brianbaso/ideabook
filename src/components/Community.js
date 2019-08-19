79c5cf59dd40e549d1f9f1722d43aa76f3c4dbc8
Brian <brianbaso@gmail.com>
2019-08-19 19:38:39 -0400

    community page has public posts

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,28 +1,59 @@
 import React from 'react';
 import { Toast, ToastBody, ToastHeader } from 'reactstrap';
+import * as firebase from "firebase/app";
+import "firebase/auth"
 
 export default class Community extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      ideas: []
     };
   }
 
+  componentDidMount() {
+    const db = firebase.firestore();
+    const dbRef = db.collection("posts");
+    let ideas = [];
+
+    // Create the ideas array and push the firestore doc id into each element
+    dbRef.get()
+      .then((snapshot) => {
+        snapshot.forEach(doc => {
+          ideas.push({ ...doc.data(), id: doc.id });
+        });
+        this.setState({ ideas : ideas });
+      })
+      .catch((e) => {
+        console.log('Error getting documents', e);
+      });
+  }
+
   render() {
-    return (
-      <div>
-        <div className="p-3 my-2 rounded">
-          <Toast>
-            <ToastHeader>
-              Community
+    const libraryCards = [];
+
+    this.state.ideas.forEach((idea) => {
+      console.log(idea);
+      libraryCards.push(
+        <div className="my-2 rounded" key={idea.id} id="libraryParentDiv">
+          <Toast id="libraryParentToast">
+            <ToastHeader id="libraryHeaderToast">
+              {idea.submissionTags[0]}
+              {idea.submissionTags[1]}
             </ToastHeader>
-            <ToastBody>
-              This is a toast on a white background — check it out!
+            <ToastBody id="libraryBodyToast">
+              {idea.content}
             </ToastBody>
           </Toast>
         </div>
+      );
+    });
+
+    return (
+      <div id="libraryContainer">
+        <h4 id="myPrivateIdeas">Community</h4>
+        {libraryCards}
       </div>
     );
   }
