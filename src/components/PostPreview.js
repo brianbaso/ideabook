4bc485c2053a09671ae3ce26af292888ebf0720c
Brian <brianbaso@gmail.com>
2019-08-31 13:26:51 -0400

    Seperate Idea into post and private idea

---
+++ src/components/PostPreview.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import {
+  Toast,
+  ToastBody,
+  ToastHeader,
+  Badge,
+  Button,
+  Collapse,
+  CardBody,
+  Card,
+  FormGroup,
+  Form,
+  Label,
+  Input
+ } from 'reactstrap';
+ import * as firebase from "firebase/app";
+ import Post from './Post.js'
+ import { Router, Route, Link } from 'react-router-dom'
+
+export default class PostPreview extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collapse: false,
+      problem: '',
+      solution: ''
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.handleProblemChange = this.handleProblemChange.bind(this);
+    this.handleSolutionChange = this.handleSolutionChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.createPost = this.createPost.bind(this);
+  }
+
+  toggle() {
+    this.setState(state => ({ collapse: !state.collapse }));
+  }
+
+  handleProblemChange(event) {
+    this.setState({
+      problem: event.target.value
+    });
+  }
+
+  handleSolutionChange(event) {
+    this.setState({
+      solution: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    // debugger;
+    // console.log('Post successfully submitted',
+    //  this.state.problem, this.state.solution);
+     this.createPost();
+     event.preventDefault();
+  }
+
+  /*
+    1. Create forms that show when 'Share' button is clicked
+    2. Fill out 'problem' and 'solution' form
+    3. Press 'Post'
+    4. Set 'content' and 'submission tags' of idea selected
+    as variables in createPost scope (or can we access props?)
+    5. Create new post and reference new variables + fields from dom
+  */
+  createPost() {
+    const user = firebase.auth().currentUser.uid;
+    const db = firebase.firestore();
+    const dbRef = db.collection("posts");
+
+    dbRef.add({
+      content: this.props.content,
+      submissionTags: this.props.submissionTags,
+      problem: this.state.problem,
+      solution: this.state.solution,
+      roles: {
+        [user]: "owner"
+      }
+    })
+    .then(() => {
+      console.log("Post successfully created");
+    })
+    .catch((e) => {
+      console.log("Error creating post: ", e);
+    });
+  }
+
+  render() {
+    const submissionTags = [];
+
+    this.props.submissionTags.forEach((tag) => {
+      submissionTags.push(
+        <Badge id="submissionTag" color="primary">{tag}</Badge>
+      );
+    })
+
+    return (
+      <div className="my-2 rounded" id="libraryParentDiv">
+        <Toast id="libraryParentToast">
+          <ToastHeader id="libraryHeaderToast">
+            {submissionTags}
+          </ToastHeader>
+          <ToastBody id="libraryBodyToast">
+            <div>
+              <Link to={{
+                pathname: `/post/${this.props.id}`,
+                state: {
+                  submissionTags: this.props.submissionTags,
+                  content: this.props.content,
+                  problem: this.props.problem,
+                  solution: this.props.solution
+                }
+              }}>
+                {this.props.content}
+              </Link>
+            </div>
+          </ToastBody>
+        </Toast>
+      </div>
+    );
+  }
+}
---
+++ src/components/PrivateIdeaPreview.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import {
+  Toast,
+  ToastBody,
+  ToastHeader,
+  Badge,
+  Button,
+  Collapse,
+  CardBody,
+  Card,
+  FormGroup,
+  Form,
+  Label,
+  Input
+ } from 'reactstrap';
+ import * as firebase from "firebase/app";
+ import { Router, Route, Link } from 'react-router-dom'
+
+export default class PrivateIdeaPreview extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collapse: false,
+      problem: '',
+      solution: ''
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.handleProblemChange = this.handleProblemChange.bind(this);
+    this.handleSolutionChange = this.handleSolutionChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.createPost = this.createPost.bind(this);
+  }
+
+  toggle() {
+    this.setState(state => ({ collapse: !state.collapse }));
+  }
+
+  handleProblemChange(event) {
+    this.setState({
+      problem: event.target.value
+    });
+  }
+
+  handleSolutionChange(event) {
+    this.setState({
+      solution: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    // debugger;
+    // console.log('Post successfully submitted',
+    //  this.state.problem, this.state.solution);
+     this.createPost();
+     event.preventDefault();
+  }
+
+  /*
+    1. Create forms that show when 'Share' button is clicked
+    2. Fill out 'problem' and 'solution' form
+    3. Press 'Post'
+    4. Set 'content' and 'submission tags' of idea selected
+    as variables in createPost scope (or can we access props?)
+    5. Create new post and reference new variables + fields from dom
+  */
+  createPost() {
+    const user = firebase.auth().currentUser.uid;
+    const db = firebase.firestore();
+    const dbRef = db.collection("posts");
+
+    dbRef.add({
+      content: this.props.content,
+      submissionTags: this.props.submissionTags,
+      problem: this.state.problem,
+      solution: this.state.solution,
+      roles: {
+        [user]: "owner"
+      }
+    })
+    .then(() => {
+      console.log("Post successfully created");
+    })
+    .catch((e) => {
+      console.log("Error creating post: ", e);
+    });
+  }
+
+  render() {
+    const submissionTags = [];
+
+    this.props.submissionTags.forEach((tag) => {
+      submissionTags.push(
+        <Badge id="submissionTag" color="primary">{tag}</Badge>
+      );
+    })
+
+    return (
+      <div className="my-2 rounded" id="libraryParentDiv">
+        <Toast id="libraryParentToast">
+          <ToastHeader id="libraryHeaderToast">
+            {submissionTags}
+          </ToastHeader>
+          <ToastBody id="libraryBodyToast">
+            <div>
+                {this.props.content}
+            </div>
+            <div>
+              <p id="share-button" onClick={this.toggle}>
+                Share
+              </p>
+              <Collapse isOpen={this.state.collapse}>
+                <Card>
+                  <CardBody>
+                    <FormGroup>
+                      <Form onSubmit={this.handleSubmit}>
+                        <Label for="exampleText">
+                          What's the problem you're solving?
+                        </Label>
+                        <Input type="textarea" name="text" id="problemText"
+                         placeholder="Enter the problem"
+                         value={this.state.problem} onChange={this.handleProblemChange}/>
+                        <Label for="exampleText">
+                          What's the solution?
+                        </Label>
+                        <Input type="textarea" name="text" id="solutionText"
+                         placeholder="Enter the solution"
+                         value={this.state.solution} onChange={this.handleSolutionChange}/>
+                         <Button id="postButton" color="primary" type="submit"
+                          outline>Post</Button>
+                      </Form>
+                    </FormGroup>
+                  </CardBody>
+                </Card>
+              </Collapse>
+            </div>
+          </ToastBody>
+        </Toast>
+      </div>
+    );
+  }
+}
