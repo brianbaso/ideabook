e88002106957c3255147eddd00e1fbdc10f4ee4a
Brian <brianbaso@gmail.com>
2019-09-01 16:09:30 -0400

    saving one comment now but overwriting

--- src/components/Post.js
+++ src/components/Post.js
@@ -14,17 +14,59 @@ import {
   Input
  } from 'reactstrap';
  import * as firebase from "firebase/app";
+let admin = require('firebase-admin');
 
 export default class Post extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      value: ''
+      value: '',
+      id: '',
+      content: '',
+      submissionTags: [],
+      problem: '',
+      solution: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.saveComment = this.saveComment.bind(this);
+  }
+
+  componentDidMount() {
+    let routeState;
+
+    // In order for this.props.location.state to persist after page refresh
+    // for example, refreshing after a comment is submit
+    // save this.props.location.state to localStorage so that it persists
+    if (this.props.location.state) {
+      console.log('routeState', this.props.location.state);
+      localStorage.setItem('routeState', JSON.stringify(this.props.location.state));
+      routeState = this.props.location.state;
+
+      this.setState({
+        id: routeState.id,
+        content: routeState.content,
+        submissionTags: routeState.submissionTags,
+        problem: routeState.problem,
+        solution: routeState.solution,
+      });
+
+    } else {
+      routeState = localStorage.getItem('routeState');
+      if (routeState) {
+        routeState = JSON.parse(routeState);
+      }
+
+      this.setState({
+        id: routeState.id,
+        content: routeState.content,
+        submissionTags: routeState.submissionTags,
+        problem: routeState.problem,
+        solution: routeState.solution,
+      });
+    }
   }
 
   handleChange(event) {
@@ -42,16 +84,16 @@ export default class Post extends React.Component {
   saveComment() {
     const user = firebase.auth().currentUser.uid;
     const db = firebase.firestore();
-    const dbRef = db.collection("posts").doc(user);
+    const dbRef = db.collection("posts").doc(this.state.id);
 
-    dbRef.update({
-      comments: db.FieldValue.arrayUnion({
-        comment: this.state.value,
+    dbRef.set({
+      comment: {
+        text: this.state.value,
         roles: {
           [user]: "owner"
         }
-      })
-    })
+      }
+    }, { merge: true })
     .then(() => {
       console.log("Document successfully written.");
     })
@@ -60,29 +102,10 @@ export default class Post extends React.Component {
     });
   }
 
-  componentDidMount() {
-    let routeState;
-
-    if (this.props.location.state) {
-      console.log('routeState', this.props.location.state);
-      localStorage.setItem('routeState', JSON.stringify(this.props.location.state));
-      routeState = this.props.location.state;
-    } else {
-      routeState = localStorage.getItem('routeState');
-      if (routeState) {
-        routeState = JSON.parse(routeState);
-      }
-    }
-  }
-
   render() {
-    const content = this.props.location.state.content;
-    const problem = this.props.location.state.problem;
-    const solution = this.props.location.state.solution;
-    const submissionTags = this.props.location.state.submissionTags;
     const tags = [];
 
-    submissionTags.forEach((tag) => {
+    this.state.submissionTags.forEach((tag) => {
       tags.push(
         <Badge id="submissionTag" color="primary">{tag}</Badge>
       );
@@ -91,7 +114,7 @@ export default class Post extends React.Component {
     return (
       <div id="libraryContainer">
         <div className="my-2 rounded" id="libraryParentDiv">
-          <h2>{content}</h2>
+          <h2>{this.state.content}</h2>
           <div id="post-subtitle-text">
             <p>August 31, 2019</p>
           </div>
@@ -99,12 +122,12 @@ export default class Post extends React.Component {
             <ToastBody id="libraryBodyToast">
               <div>
                 <p id="prob-sol-title">Problem</p>
-                <p id="prob-sol-text">{problem}</p>
+                <p id="prob-sol-text">{this.state.problem}</p>
               </div>
               <hr id="horizontal-break"/>
               <div>
                 <p id="prob-sol-title">Solution</p>
-                <p id="prob-sol-text">{solution}</p>
+                <p id="prob-sol-text">{this.state.solution}</p>
               </div>
               <div id="post-submission-tags">
                 {tags}
